3c95ddb4626b301ee430fcf5ef01e26128f2f30e
Abdelrahman Ayman <55602893+AbdelrahmanAyman48@users.noreply.github.com>
2024-05-28 09:35:16 +0300

    2

---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+// @ts-ignore
+import styles from "../css/Title.module.css";
+function Title() {
+  return <h1 className={styles.title}>Calorie Tracker</h1>;
+}
+export default Title;
